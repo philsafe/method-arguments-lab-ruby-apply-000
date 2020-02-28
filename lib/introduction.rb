0e09389546f2dca226854625ff015b7a06fc869e
Philip Phipps <phippsphilip@yahoo.com>
2020-02-28 20:22:17 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,7 @@
+def introduction (phil)
+  puts "Hi, my name is #{phil}."
+end
+
+def introduction_with_language (phil,ruby)
+  puts "Hi, my name is #{phil} and I am learning to program in #{ruby}."
+end
