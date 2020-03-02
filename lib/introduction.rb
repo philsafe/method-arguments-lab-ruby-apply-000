0880290fc2edc68bd0dba257531a3f77405a1ffe
Philip Phipps <phippsphilip@yahoo.com>
2020-03-02 21:29:00 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,5 +3,5 @@ def introduction (phil)
 end
 
 def introduction_with_language (phil,ruby)
-  puts "Hi, my name is #{phil} and I am learning to program in #{ruby}.\n"
+  puts "Hi, my name is #{phil} and I am learning to program in #{ruby}."
 end
